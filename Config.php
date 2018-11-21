3af3828000af6956daa15d542ca37ffd851eb5a8
asnatariq <44018932+asnatariq@users.noreply.github.com>
2018-11-21 10:58:02 +0500

    Add files via upload

---
+++ Config.php
@@ -0,0 +1,8 @@
+<?php
+$databaseHost = 'localhost';
+$databaseName = 'asna';
+$databaseUsername = 'root';
+$databasePassword = "";
+ 
+$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
+?>
